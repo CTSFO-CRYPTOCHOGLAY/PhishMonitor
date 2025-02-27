2e80079192eb5a87cc672b7508da862594c96722
Mohammed  Choglay <72378816+CTSFO-CRYPTOCHOGLAY@users.noreply.github.com>
2025-02-27 01:49:41 +0000

    Add files via upload
    Grabbing data from open phish works. 
    he list updated every 12 hours. This was checked by using the hashing utils within the script for the reports saved as this feature is functioning. 
    Started the Virus Total implementation. 
    .env file implemented.

--- Intel-Phish.py
+++ Intel-Phish.py
@@ -1,61 +1,71 @@
-#Intel Phish by Mohammed Choglay
-import urllib.request 
+# Intel Phish by Mohammed Choglay
+import urllib.request
 from urllib.request import urlopen
-#import re (Not in use as of yet)
+
+# import re (Not in use as of yet)
 import os
 import hashlib
 from collections import defaultdict
+from dotenv import load_dotenv
+import time
+
+
+# Data being loaded from .env
+load_dotenv()
+Open_Phish_URL = os.getenv("Open_Phish_URL")
+directory = os.getenv("Open_Phish_Report_Path")
+VT_API = os.getenv("VT_API")
+
 
 print("[*] Starting Intel Phish")
 print("[*] Gathering infromation from OpenPhish")
 
-Open_Phish_URL="https://raw.githubusercontent.com/openphish/public_feed/refs/heads/main/feed.txt"
-Open_Phish_URL_Path = "A:\\DEFSEC\\Bespoke Tools\\phinshing intel\\Reports\\"
 
 def OpenPhish_Status_Check(Open_Phish_URL):
-    try: 
+    try:
         response = urllib.request.urlopen(Open_Phish_URL)
-        if  response.getcode() == 200:
-            print("[*] " + str(Open_Phish_URL) + " is online")#
+        if response.getcode() == 200:
+            print("[*] " + str(Open_Phish_URL) + " is online")  #
             print("\n")
 
             with urlopen(Open_Phish_URL) as webpage:
                 phish_data = webpage.read().decode()
             print(phish_data)
-        
-        save_data_txt = input("[*] Do you want to save the Y/N: ")
+
+        save_data_txt_name = input("[*] Do you want to save the Y/N: ")
         save_data(save_data_txt_name)
-        
 
     except Exception as e:
         print(f"[*] Error: {e}")
         return False
 
+
 def save_data(save_data_txt):
     if save_data_txt == "Y":
-        file_name=input("[*] Enter file name: ")
+        file_name = input("[*] Enter file name: ")
         with urlopen(Open_Phish_URL) as webpage:
-                phish_data_wb = webpage.read()
+            phish_data_wb = webpage.read()
 
-        with open(Open_Phish_URL_Path + file_name, 'wb' ) as download:
-                download.write(phish_data_wb)
-    elif save_data_txt == "N": 
-            print("false")
-    else: 
+        with open(directory + file_name, "wb") as download:
+            download.write(phish_data_wb)
+    elif save_data_txt == "N":
+        print("[*] Program closed.")
+    else:
         print("[!] Invalid Input")
 
 
-def hashing(Open_Phish_URL_Path, algorithm="sha256"): 
+def hashing(Open_Phish_URL_Path, algorithm="sha256"):
     hashing = hashlib.new(algorithm)
     with open(Open_Phish_URL_Path, "rb") as f:
         while chunk := f.read(8192):
             hashing.update(chunk)
     return hashing.hexdigest()
 
+
 def scan_directory(directory, algorithm="sha256"):
     """Scans a directory, hashes files, and counts occurrences of hashes."""
     if not os.path.exists(directory):
-        print(f"Error: Directory '{directory}' does not exist.")
+        print(f"[!] Error: Directory '{directory}' does not exist.")
         return
 
     hash_count = defaultdict(int)  # Stores hash counts
@@ -79,6 +89,20 @@ def scan_directory(directory, algorithm="sha256"):
         print(f"{file_hash}: {count} time(s)")
 
 
+def url_scan_virustotal(url):
+    url_scan = "https://www.virustotal.com/api/v3/urls"
+    headers = {"x-apikey": VT_API}
+    data = {"url": url}
+
+    try:
+        response = requests.post(url_scan, headers=headers, data=data)
+        if response.status_code == 200:
+            return response.json()["data"]["id"]
+        else:
+            return None
+    except Exception as e:
+        return None
+
 
 #OpenPhish_Status_Check(Open_Phish_URL)
-scan_directory(Open_Phish_URL_Path)
+scan_directory(directory)
